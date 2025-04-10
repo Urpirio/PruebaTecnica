fb8b044f04e413b093064b2b8b01e3ac0c58e6da
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-10 09:09:46 -0400

    Meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,6 +8,7 @@ export default function layout({children}) {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>UCars</title>
+        <meta name="description" content="Prueba Tecnica para OneMax" />
     </head>
         <body>
           <Header/>
